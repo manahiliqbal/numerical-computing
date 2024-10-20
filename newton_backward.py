01cf0f14774e78e732c6c5e7aba69c1c69d90a5a
manahil <120031852+manahiliqbal@users.noreply.github.com>
2024-10-20 12:51:39 +0500

    newton backward

---
+++ newton_backward.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+
+x = [0, 0.25, 0.5, 0.75]
+y = [1, 1.64872, 2.71828, 4.48169]
+n = len(x)
+X = 0.43
+h = x[1] - x[0]
+F = np.zeros((n,n))
+
+for i in range(n):
+    F[i,0] = y[i]
+    
+for i in range(1,n):
+    for j in range(1, i+1):
+        F[i,j] = F[i,j-1] - F[i-1,j-1] 
+        
+P = (X - x[n-1])/h      
+m = np.zeros((n,1))
+m[0] = 1
+S = F[n-1,0] 
+for i in range(1,n):
+    m[i] = m[i-1]*(P+i-1)  
+    S += m[i]*(F[n-1,i]/math.factorial(i)) 
+    
+print(S)    
